b6324efcedafa7a918e6c4fe5d42930b91dea796
Yogeswaran S <yogeswaran3627@gmail.com>
2026-08-19 23:18:53 +0530

    Time: 34 ms (61.62%), Space: 99.9 MB (53.21%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,33 @@
+class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+
+        if (minStack.empty() || value <= minStack.peek()) {
+            minStack.push(value);
+        }
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
